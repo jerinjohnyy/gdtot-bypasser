bae7296625fcf3488669cfb34c492826dc32e7c9
jerinjohnyy <118479746+jerinjohnyy@users.noreply.github.com>
2023-01-04 23:21:33 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ app.run(
     host=environ.get("HOST", "0.0.0.0"),
     port=environ.get("PORT", 8000),
     load_dotenv=True,
-    debug=(True if environ.get("DEBUG").lower() == "true" else False),
+    debug=False,
 )
